fa509105b0bb60f1195d53ac2075978c48d53310
Mrkol <roma.sandu@gmail.com>
2018-03-19 01:45:34 +0300

    First test files. No test yet.

---
+++ common_tests/src/component_tests.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "Components\health.hpp"
+#include "Components\mana.hpp"
+#include "Components\position.hpp"
+#include "Components\team.hpp"
+
--- common_tests/src/entity_system_tests.cpp
+++ common_tests/src/entity_system_tests.cpp
@@ -3,8 +3,3 @@
 #include "EntityInstance.hpp"
 #include "EntityType.hpp"
 #include "EntityTypeBuilder.hpp"
-
-TEST(MyFunCase, TestIsTrue)
-{
-	ASSERT_TRUE(true);ASSERT_TRUE(true);
-}
